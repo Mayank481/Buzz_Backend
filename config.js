619e37b2f5e60fa9bc16e06ac2fa7ad9a15973fd
Mayank jaggi <69957160+Mayank481@users.noreply.github.com>
2024-05-21 18:07:07 +0530

    Update config.js
    add database url

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'dev') {
   exports.PORT = process.env.PORT || 5000;
   exports.API_URL = 'http://localhost:' + this.PORT;
   exports.CLIENT_URL = 'http://localhost:3000';
-  exports.MONGO_URI = 'mongodb+srv://prashant24:Prince24@cluster0.2pd6v.mongodb.net/buzz?retryWrites=true&w=majority';
+  exports.MONGO_URI = 'mongodb+srv://web:web123@bookstore.vpugutg.mongodb.net/buzzapp';
   exports.G_CLIENT_SECRET = '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';
   exports.G_CLIENT_ID =
     '567446151772-90gk2erb8fsrtj6u6pivdsr3r7js66bc.apps.googleusercontent.com';
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'test') {
   exports.API_URL = 'http://localhost:' + this.PORT;
   exports.CLIENT_URL = 'http://localhost:3000';
   exports.MONGO_URI =
-  'mongodb+srv://prashant24:Prince24@cluster0.2pd6v.mongodb.net/buzz?retryWrites=true&w=majority';
+  'mongodb+srv://web:web123@bookstore.vpugutg.mongodb.net/buzzapp';
   exports.G_CLIENT_SECRET = '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';
   exports.G_CLIENT_ID =
     '567446151772-90gk2erb8fsrtj6u6pivdsr3r7js66bc.apps.googleusercontent.com';
